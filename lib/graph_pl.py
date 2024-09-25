e6757453f5afa5df65b13e73479d2a51cce78945
Parker Lauders <parkerlauders04@gmail.com>
2024-09-25 14:33:51 -0400

    security: Improve properities based on new requirements

--- lib/graph_pl.py
+++ lib/graph_pl.py
@@ -3,6 +3,29 @@ import lib.matrix as matrix
 
 class Graph(object):
     """This class contains graph operations"""
+
+    def __init__(self, graph_input=None) -> None:
+        """
+        The constructor for the Graph class
+        :param *args: A adjacency list or matrix
+        """
+        self._adj_list = None
+        self._adj_matrix = None
+        
+        # Check if a adjacency list was supplied
+        if isinstance(graph_input, dict): 
+            # Set the adjacency list
+            self._adj_list = graph_input
+            
+            # Convert the list to matrix
+            self._adj_list_to_matrix()
+        # Check if a adjacency matrix was supplied
+        elif isinstance(graph_input, matrix.Matrix):
+            # Set the adjacency matrix
+            self._adj_matrix = graph_input
+            
+            # Convert the matrix to a list
+            self._adj_matrix_to_list()
             
     @property
     def adj_list(self) -> vector:
@@ -11,14 +34,6 @@ class Graph(object):
         :return: Adjacency list
         """
         return self._adj_list
-    
-    @adj_list.setter
-    def adj_list(self, adj_list) -> None:
-        """
-        Setter for the adjacency list
-        :param adj_list: The adjacency list to set
-        """
-        self._adj_list = adj_list
         
     @property
     def adj_matrix(self) -> matrix.Matrix:
@@ -27,14 +42,6 @@ class Graph(object):
         :return: Adjacency matrix
         """
         return self._adj_matrix
-    
-    @adj_matrix.setter
-    def adj_matrix(self, adj_matrix):
-        """
-        Setter for the adjacency matrix
-        :param adj_matrix: The adjacency matrix to set
-        """
-        self._adj_matrix = adj_matrix
         
     @property
     def order(self) -> int:
@@ -42,15 +49,14 @@ class Graph(object):
         Getter for the graph order
         :return: The order of the graph
         """
-        return self._order
-    
-    @order.setter
-    def order(self, value) -> None:
-        """
-        Setter for the graph order
-        :param value: The desired order of the graph
-        """
-        self._order = value
+        # Check if there is a list
+        if self.adj_list:
+            return len(list(self.adj_list.keys()))
+        # Check if there is a matrix
+        elif self.adj_matrix:
+            return self._adj_matrix.num_rows
+        else:
+            return 0
         
     @property
     def size(self) -> int:
@@ -58,52 +64,15 @@ class Graph(object):
         Getter for the graph size
         :return: The size of the graph
         """
-        return self._size
-    
-    @size.setter
-    def size(self, value) -> None:
-        """
-        Setter for the graph size
-        :param value: The desired size of the graph
-        """
-        self._size = value
-        
-    def __init__(self, graph_input=None) -> None:
-        """
-        The constructor for the Graph class
-        :param *args: A adjacency list or matrix
-        """
-        self._adj_list = None
-        self._adj_matrix = None
-        self._order = 0
-        self._size = 0
-        
-        # Check if a adjacency list was supplied
-        if isinstance(graph_input, dict): 
-            # Set the adjacency list
-            self._adj_list = graph_input
-            
-            # Get the order
-            self._get_order() 
-            
-            # Convert the list to matrix
-            self._adj_list_to_matrix()
-            
-            # Get the size of the graph
-            self._get_size()
-        # Check if a adjacency matrix was supplied
-        elif isinstance(graph_input, matrix.Matrix):
-            # Set the adjacency matrix
-            self._adj_matrix = graph_input
-            
-            # Get the order of the graph
-            self._get_order() 
-            
-            # Convert the matrix to a list
-            self._adj_matrix_to_list()
-
-            # Get the size of the graph
-            self._get_size()
+        size = 0
+        # Check if there is a list
+        if self.adj_list:
+            for _, edges in self._adj_list.items():
+                size += len(edges)
+                
+            return size / 2
+        else:
+            return 0
             
     def _adj_list_to_matrix(self) -> None:
         """
@@ -117,8 +86,8 @@ class Graph(object):
         matrix_blueprint = []
         
         # Initialize an empty matrix
-        for _ in range(self._order):
-            column_data = [0 for _ in range(self._order)]
+        for _ in range(self.order):
+            column_data = [0 for _ in range(self.order)]
             row_data = vector.Vector(*column_data)
             matrix_blueprint.append(row_data)
             
@@ -127,7 +96,7 @@ class Graph(object):
         
         # Convert the adjacency list to matrix
         for key, data in self._adj_list.items():
-            for element in range(self._order - 1):
+            for element in range(self.order - 1):
                 self._adj_matrix.__setitem__([key - 1, int(data.__getitem__(element)) - 1], 1)
     
     def _adj_matrix_to_list(self) -> None:
@@ -142,9 +111,9 @@ class Graph(object):
         adj_list_blueprint = {}
         
         # Break down the matrix
-        for row in range(self._order):
+        for row in range(self.order):
             edge_data = []
-            for column in range(self._order):
+            for column in range(self.order):
                 # Check if there is a edge at the current matrix position
                 if self._adj_matrix.__getitem__([row, column]):
                     edge_data.append(column + 1)
@@ -154,32 +123,6 @@ class Graph(object):
             
         # Create the list
         self._adj_list = adj_list_blueprint
-    
-    def _get_order(self) -> None:
-        """
-        Calculates the order of a graph
-        """
-        # Check if there is a list
-        if self.adj_list:
-            self._order = len(list(self.adj_list.keys()))
-        # Check if there is a matrix
-        elif self.adj_matrix:
-            self._order = self._adj_matrix.num_rows
-        else:
-            raise Exception("Unable to get the order due to no adjacency list or matrix being found!")
-    
-    def _get_size(self) -> None:
-        """
-        Calculates the size of a graph
-        """
-        # Check if there is a list
-        if self.adj_list:
-            for _, edges in self._adj_list.items():
-                self._size += len(edges)
-                
-            self._size = self._size / 2
-        else:
-            raise Exception("Unable to get the size due to no adjacency list being found!")
     
     def get_vertex_weight(self):
         """
