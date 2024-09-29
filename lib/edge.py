c7c2286168db75bc812972877019fc333c66e6e2
Parker Lauders <parkerlauders04@gmail.com>
2024-09-29 18:20:36 -0400

    Add: ==/hash operator, update method commits

--- lib/edge.py
+++ lib/edge.py
@@ -11,7 +11,7 @@ class Edge:
     @property
     def start_vertex(self):
         """
-        Get the start of an edge
+        Get the start vertex of the edge
         :return: The start vertex object
         """
         return self._start
@@ -19,7 +19,7 @@ class Edge:
     @property
     def end_vertex(self):
         """
-        Get the end of an edge
+        Get the end vertex of the edge
         :return: The end vertex object
         """
         return self._end
@@ -27,7 +27,7 @@ class Edge:
     @property
     def weight(self):
         """
-        Get the weight of an edge
+        Get the weight of the edge
         :return: The edge weight
         """
         return self._weight
@@ -35,21 +35,46 @@ class Edge:
     @weight.setter
     def weight(self, value):
         """
-        Set the end of an edge
-        :param value: The edge weight
+        Set the weight of the edge
+        :param value: The new edge weight
         """
         self._weight = value
     
     @property
     def directed(self):
         """
-        Set the end of an edge
-        :param vertex: The vertex object
+        Check if the edge is directed
+        :return: True if the edge is directed, False otherwise
         """
         return self._directed
     
+    def __eq__(self, other):
+        """
+        Check if this edge is equal to another edge
+        :param other: The other edge to compare
+        :return: True if edges are equal, False otherwise
+        """
+        if not isinstance(other, Edge):
+            return NotImplemented
+        return (self.start_vertex == other.start_vertex and
+                self.end_vertex == other.end_vertex and
+                self.weight == other.weight and
+                self.directed == other.directed)
+    
+    def __hash__(self):
+        """
+        Compute the hash of the edge
+        :return: The hash value
+        """
+        return hash((self.start_vertex, self.end_vertex, self.weight, self.directed))
+    
     def __repr__(self) -> str:
+        """
+        Return the string representation of the edge
+        :return: A string representing the edge
+        """
+        direction = "->" if self.directed else "--"
         if self.weight is None:
-            return f"Edge({self.start_vertex.id}->{self.end_vertex.id})"
+            return f"Edge({self.start_vertex.id}{direction}{self.end_vertex.id})"
         else:
-            return f"Edge({self.start_vertex.id}->{self.end_vertex.id}) [{self.weight}]"
+            return f"Edge({self.start_vertex.id}{direction}{self.end_vertex.id}) [{self.weight}]"
